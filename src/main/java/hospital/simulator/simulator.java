bd0e3c0e9ad6940d3e554bd0efb0d8f9efb262b1
DiegoAP5 <diegoalv.n@gmail.com>
2023-11-26 21:25:00 -0600

    actors added

--- src/main/java/hospital/simulator/simulator.java
+++ src/main/java/hospital/simulator/simulator.java
@@ -1,5 +1,6 @@
 package hospital.simulator;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -12,6 +13,8 @@ import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class simulator extends Application {
     @Override
@@ -23,12 +26,14 @@ public class simulator extends Application {
         List<Consultorio> consultorios = createConsultationRooms();
         consultorios.forEach(c -> mainLayout.getChildren().add(c.getVisualRepresentation()));
 
-        List<Rectangle> patients = createPatients(10);
-        for (int i = 0; i < patients.size(); i++) {
-            Rectangle patient = patients.get(i);
-            mainLayout.getChildren().add(patient);
-            animatePatientToReception(patient, receptionist, consultorios, 5 * i); // Retraso secuencial
-        }
+        Pane doctorArea = createDoctorArea();
+        mainLayout.getChildren().add(doctorArea);
+
+        Pane nurseArea = createNurseArea();
+        mainLayout.getChildren().add(nurseArea);
+
+        PatientMonitor monitor = new PatientMonitor(10);
+        schedulePatientCreation(monitor, mainLayout, receptionist, consultorios);
 
         Scene scene = new Scene(mainLayout, 800, 600);
         primaryStage.setTitle("Hospital Simulator");
@@ -36,11 +41,57 @@ public class simulator extends Application {
         primaryStage.show();
     }
 
+    private Pane createNurseArea() {
+        Pane nurseArea = new Pane();
+        nurseArea.setLayoutX(750); // A la derecha de la ventana
+        nurseArea.setLayoutY(200);
+
+        for (int i = 0; i < 5; i++) {
+            Rectangle nurse = new Rectangle(15, 15, Color.GREEN);
+            nurse.setLayoutX(0);
+            nurse.setLayoutY(15 * i);
+            nurseArea.getChildren().add(nurse);
+        }
+
+        return nurseArea;
+    }
+
     private Rectangle createReceptionist() {
-        Rectangle receptionist = new Rectangle(20, 20, Color.YELLOW);
+        Rectangle receptionist = new Rectangle(50, 20, Color.YELLOW); // Rectángulo más ancho
         receptionist.setLayoutX(10); // Posición de la recepcionista
         receptionist.setLayoutY(200);
         return receptionist;
+    }
+
+    private Pane createDoctorArea() {
+        Pane doctorArea = new Pane();
+        doctorArea.setLayoutX(300); // Posicionamiento en el centro inferior
+        doctorArea.setLayoutY(500);
+
+        for (int i = 0; i < 15; i++) {
+            Rectangle doctor = new Rectangle(15, 15, Color.RED);
+            doctor.setLayoutX(15 * i);
+            doctor.setLayoutY(0);
+            doctorArea.getChildren().add(doctor);
+        }
+
+        return doctorArea;
+    }
+
+    public class PatientMonitor {
+        private final int totalPatients;
+        private int currentPatient = 0;
+
+        public PatientMonitor(int totalPatients) {
+            this.totalPatients = totalPatients;
+        }
+
+        public synchronized void createAndAnimateNextPatient(Pane mainLayout, Rectangle receptionist, List<Consultorio> consultorios) {
+            if (currentPatient < totalPatients) {
+                createAndAnimatePatient(currentPatient, mainLayout, receptionist, consultorios);
+                currentPatient++;
+            }
+        }
     }
 
     private List<Rectangle> createPatients(int numberOfPatients) {
@@ -80,8 +131,8 @@ public class simulator extends Application {
 
     private List<Consultorio> createConsultationRooms() {
         List<Consultorio> consultationRooms = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            Consultorio consultorio = new Consultorio(100 + 50 * i, 300); // Posicionamiento en el centro
+        for (int i = 0; i < 50; i++) {
+            Consultorio consultorio = new Consultorio(50 + (i % 10) * 60, 30 + (i / 10) * 60); // Ajuste de posicionamiento
             consultationRooms.add(consultorio);
         }
         return consultationRooms;
@@ -105,6 +156,28 @@ public class simulator extends Application {
                 break;
             }
         }
+    }
+
+    private void createAndAnimatePatient(int patientNumber, Pane mainLayout, Rectangle receptionist, List<Consultorio> consultorios) {
+        Rectangle patient = new Rectangle(20, 20, Color.BLUE);
+        patient.setLayoutX(10); // Posición inicial X (esquina superior izquierda)
+        patient.setLayoutY(10); // Posición inicial Y
+        mainLayout.getChildren().add(patient);
+
+        animatePatientToReception(patient, receptionist, consultorios, 5 * patientNumber);
+    }
+
+    private void schedulePatientCreation(PatientMonitor monitor, Pane mainLayout, Rectangle receptionist, List<Consultorio> consultorios) {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(5000); // Esperar 5 segundos
+                    Platform.runLater(() -> monitor.createAndAnimateNextPatient(mainLayout, receptionist, consultorios));
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }).start();
     }
 
     private void animatePatientToConsultorio(Rectangle patient, Consultorio consultorio) {
