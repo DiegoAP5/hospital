4cf0437939c8b03417a3f9c324cc527262c50fe7
DiegoAlbertoPerezJuarez <213475@ids.upchiapas.edu.mx>
2023-11-26 18:03:36 -0600

    movement

--- src/main/java/hospital/simulator/simulator.java
+++ src/main/java/hospital/simulator/simulator.java
@@ -1,7 +1,6 @@
 package hospital.simulator;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -11,95 +10,109 @@ import javafx.scene.shape.Rectangle;
 import javafx.animation.TranslateTransition;
 import javafx.util.Duration;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class simulator extends Application {
     @Override
     public void start(Stage primaryStage) {
         Pane mainLayout = new Pane();
-        mainLayout.getChildren().add(createReceptionArea());
-        mainLayout.getChildren().add(createConsultationRooms());
+        Rectangle receptionist = createReceptionist();
+        mainLayout.getChildren().add(receptionist);
 
-        Rectangle patient = createPatient();
-        mainLayout.getChildren().add(patient);
+        List<Consultorio> consultorios = createConsultationRooms();
+        consultorios.forEach(c -> mainLayout.getChildren().add(c.getVisualRepresentation()));
+
+        List<Rectangle> patients = createPatients(10);
+        for (int i = 0; i < patients.size(); i++) {
+            Rectangle patient = patients.get(i);
+            mainLayout.getChildren().add(patient);
+            animatePatientToReception(patient, receptionist, consultorios, 5 * i); // Retraso secuencial
+        }
 
         Scene scene = new Scene(mainLayout, 800, 600);
         primaryStage.setTitle("Hospital Simulator");
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        animatePatientToReception(patient, createReceptionArea());
     }
 
-    private Pane createConsultationRooms() {
-        Pane consultationRooms = new Pane();
+    private Rectangle createReceptionist() {
+        Rectangle receptionist = new Rectangle(20, 20, Color.YELLOW);
+        receptionist.setLayoutX(10); // Posición de la recepcionista
+        receptionist.setLayoutY(200);
+        return receptionist;
+    }
+
+    private List<Rectangle> createPatients(int numberOfPatients) {
+        List<Rectangle> patients = new ArrayList<>();
+        for (int i = 0; i < numberOfPatients; i++) {
+            Rectangle patient = new Rectangle(20, 20, Color.BLUE);
+            patient.setLayoutX(10); // Posición inicial X (esquina superior izquierda)
+            patient.setLayoutY(10 + 30 * i); // Posición inicial Y, ligeramente desplazados
+            patients.add(patient);
+        }
+        return patients;
+    }
+
+    class Consultorio {
+        Rectangle visualRepresentation;
+        boolean isOccupied;
+
+        Consultorio(int x, int y) {
+            visualRepresentation = new Rectangle(30, 30);
+            visualRepresentation.setLayoutX(x);
+            visualRepresentation.setLayoutY(y);
+            isOccupied = false;
+        }
+
+        Rectangle getVisualRepresentation() {
+            return visualRepresentation;
+        }
+
+        boolean isOccupied() {
+            return isOccupied;
+        }
+
+        void setOccupied(boolean occupied) {
+            isOccupied = occupied;
+        }
+    }
+
+    private List<Consultorio> createConsultationRooms() {
+        List<Consultorio> consultationRooms = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            Rectangle consultorio = new Rectangle(30, 30);
-            consultorio.setLayoutX(150 + 40 * i); // Posicionamiento horizontal
-            consultorio.setLayoutY(250); // Posicionamiento vertical en el centro
-            consultationRooms.getChildren().add(consultorio);
+            Consultorio consultorio = new Consultorio(100 + 50 * i, 300); // Posicionamiento en el centro
+            consultationRooms.add(consultorio);
         }
         return consultationRooms;
     }
 
-    private Pane createMainLayout() {
-        BorderPane mainLayout = new BorderPane();
-        mainLayout.setTop(createPatientArrivalArea());
-        mainLayout.setLeft(createReceptionArea());
-        mainLayout.setCenter(createConsultationRooms());
-        mainLayout.setRight(createNurseWaitingArea());
-        return mainLayout;
-    }
-
-    private VBox createPatientArrivalArea() {
-        VBox patientArrivalArea = new VBox(10);
-        patientArrivalArea.getChildren().add(new Label("Área de Llegada de Pacientes"));
-        // Aquí puedes añadir más componentes como listas de pacientes, etc.
-        return patientArrivalArea;
-    }
-
-    private Rectangle createReceptionArea() {
-        Rectangle receptionArea = new Rectangle(100, 50); // Tamaño del área de recepción
-        receptionArea.setLayoutX(10); // Posición X
-        receptionArea.setLayoutY(200); // Posición Y (centro izquierda)
-        return receptionArea;
-    }
-
-    private VBox createNurseWaitingArea() {
-        VBox nurseWaitingArea = new VBox(10);
-        nurseWaitingArea.getChildren().add(new Label("Área de Espera de Enfermeros"));
-        // Espacio para mostrar enfermeros en espera o acciones relacionadas
-        return nurseWaitingArea;
-    }
-
-    private Rectangle createPatient() {
-        Rectangle patient = new Rectangle(20, 20, Color.BLUE);
-        patient.setLayoutX(750); // Posición inicial X (esquina superior derecha)
-        patient.setLayoutY(10); // Posición inicial Y
-        return patient;
-    }
-
-    private void animatePatientToReception(Rectangle patient, Rectangle receptionArea) {
+    private void animatePatientToReception(Rectangle patient, Rectangle receptionist, List<Consultorio> consultorios, int delay) {
         TranslateTransition toReception = new TranslateTransition(Duration.seconds(3), patient);
-        toReception.setToX(receptionArea.getLayoutX() - patient.getLayoutX());
-        toReception.setToY(receptionArea.getLayoutY() - patient.getLayoutY());
+        toReception.setDelay(Duration.seconds(delay));
+        toReception.setToX(receptionist.getLayoutX() - patient.getLayoutX());
+        toReception.setToY(receptionist.getLayoutY() - patient.getLayoutY());
 
-        toReception.setOnFinished(event -> animatePatientToConsultorio(patient));
+        toReception.setOnFinished(event -> assignConsultorio(patient, consultorios));
         toReception.play();
     }
 
-    private void animatePatientToConsultorio(Rectangle patient) {
-        // Escoger un consultorio al azar
-        int consultorioIndex = new Random().nextInt(10);
-        double consultorioX = 150 + 40 * consultorioIndex;
-        double consultorioY = 250;
-
-        TranslateTransition toConsultorio = new TranslateTransition(Duration.seconds(3), patient);
-        toConsultorio.setToX(consultorioX - patient.getLayoutX());
-        toConsultorio.setToY(consultorioY - patient.getLayoutY());
-        toConsultorio.play();
+    private void assignConsultorio(Rectangle patient, List<Consultorio> consultorios) {
+        for (Consultorio consultorio : consultorios) {
+            if (!consultorio.isOccupied()) {
+                animatePatientToConsultorio(patient, consultorio);
+                consultorio.setOccupied(true);
+                break;
+            }
+        }
     }
 
+    private void animatePatientToConsultorio(Rectangle patient, Consultorio consultorio) {
+        TranslateTransition toConsultorio = new TranslateTransition(Duration.seconds(3), patient);
+        toConsultorio.setToX(consultorio.visualRepresentation.getLayoutX() - patient.getLayoutX());
+        toConsultorio.setToY(consultorio.visualRepresentation.getLayoutY() - patient.getLayoutY());
+        toConsultorio.play();
+    }
 
     public static void main(String[] args) {
         launch(args);
